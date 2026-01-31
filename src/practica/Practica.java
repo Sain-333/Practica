4a3dcc6ea8a35d25f9a3bd202bc6102ec29865f9
Alvaro Alonzo <alvaroalonzo909@gmail.com>
2026-01-31 17:31:40 -0400

    Se añade la frase como estas ?

--- src/practica/Practica.java
+++ src/practica/Practica.java
@@ -8,6 +8,8 @@ public class Practica {
     public static void main(String[] args) {
         System.out.println("Hola mundo");  
         
+        System.out.println("Como estas ?");
+        
         
     }
     
