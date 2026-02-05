38c7575db55ae39b36a04401bbe610db10295017
Alvaro Alonzo <alvaroalonzo909@gmail.com>
2026-02-05 10:39:38 -0400

    Se añade el default

--- src/practica/Practica.java
+++ src/practica/Practica.java
@@ -47,6 +47,9 @@ public class Practica {
                 break;
                 
                 
+            default: System.out.println("Error, operacion incorrecta");
+                break;
+                
         }
         
         
