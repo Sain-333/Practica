dbb56a32435f12ae0d94994aaafe1e63c2a16cbc
Alvaro Alonzo <alvaroalonzo909@gmail.com>
2026-02-04 23:09:22 -0400

    Añaden variables principales

--- src/practica/Practica.java
+++ src/practica/Practica.java
@@ -1,11 +1,18 @@
 
 package practica;
 
+import java.util.Scanner;
+
 
 public class Practica {
 
     
     public static void main(String[] args) {
+        Scanner entrada = new Scanner (System.in);
+        int numero1,numero2,resultado;
+        
+        
+        
         
         
         
