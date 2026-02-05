5c5b03753f836103af2cfea72cc72ed41658f08b
Alvaro Alonzo <alvaroalonzo909@gmail.com>
2026-02-05 09:22:51 -0400

    Se imprime el resultado

--- src/practica/Practica.java
+++ src/practica/Practica.java
@@ -11,15 +11,15 @@ public class Practica {
         Scanner entrada = new Scanner (System.in);
         int numero1,numero2,resultado;
         
-        System.out.println("Digite el primer numero:");
+        System.out.print("Digite el primer numero:");
         numero1 = entrada.nextInt();
         
-        System.out.println("Digite el segundo numero:");
+        System.out.print("Digite el segundo numero:");
         numero2 = entrada.nextInt();
         
         resultado = numero1 + numero2;
         
-        
+        System.out.println("El resultado es: "+resultado);
         
     }
     
