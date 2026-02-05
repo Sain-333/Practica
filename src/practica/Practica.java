c2c3580b8967177a2a548a1bc7e633199e1e71d9
Alvaro Alonzo <alvaroalonzo909@gmail.com>
2026-02-05 10:17:25 -0400

    Se añade variable para una nueva funcion junto a su menu

--- src/practica/Practica.java
+++ src/practica/Practica.java
@@ -2,6 +2,7 @@
 package practica;
 
 import java.util.Scanner;
+import javax.swing.JOptionPane;
 
 
 public class Practica {
@@ -9,7 +10,7 @@ public class Practica {
     
     public static void main(String[] args) {
         Scanner entrada = new Scanner (System.in);
-        int numero1,numero2,resultado;
+        int numero1,numero2,operacion,resultado;
         
         System.out.print("Digite el primer numero:");
         numero1 = entrada.nextInt();
@@ -17,9 +18,18 @@ public class Practica {
         System.out.print("Digite el segundo numero:");
         numero2 = entrada.nextInt();
         
-        resultado = numero1 + numero2;
+        operacion = Integer.parseInt(JOptionPane.showInputDialog("Digite una operacion: \n"
+                + "1. Sumar \n"
+                + "2. Restar \n"
+                + "3. Multiplicar \n"
+                + "4. Dividir "));
         
-        System.out.println("El resultado es: "+resultado);
+        
+        
+        
+        
+        
+        
         
     }
     
